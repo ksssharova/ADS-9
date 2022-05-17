9447201ef795780ad1ae758f06f1ce68b7ee0659
Ксения <94642483+ksssharova@users.noreply.github.com>
2022-05-17 14:52:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,4 +7,23 @@
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+  BST<std::string> bst;
+  std::ifstream file(filename);
+  std::string word = "";
+  if (!file.is_open()) {
+    throw std::string("Error! Can not open file!");
+  }
+  while (!file.eof()) {
+    word = "";
+    while (true) {
+      char dop = file.get();
+      if ((dop >= 65 && dop <= 90) || (dop >= 97 && dop <= 122)) {
+        word += tolower(dop);
+      } else {
+        break;
+      }
+    }
+    bst.Add(word);
+  }
+  return bst;
 }
